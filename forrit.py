2813e708c3dfd86f62db55bfd9f7169d06ba7de2
Sigfús Már Viðarsson <sigfusvi227@nemi.tskoli.is>
2018-01-15 12:40:58 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -3,7 +3,18 @@ import os
 
 @route('/')
 def index():
-  return "Hallo bottlepy!"
+  return "<a href='/About'>About</a> <a href='/Biography'>Biography</a> <a href='/Pictures'>Pictures</a>"
+@route('/About')
+def index():
+  return "Hér eru upplýsingar um Steve Jobs"
+@route('/Biography')
+def index():
+  return "Hér er Biography frá Steve Jobs"
+@route('/Pictures')
+def index():
+  return "Hérna eru myndir frá lífi Steve Jobs"
+
+
   
-run(host="0.0.0.0", port=os.environ.get('PORT'))
-#run(host="localhost", port=8080)
+#run(host="0.0.0.0", port=os.environ.get('PORT'))
+run(host="localhost", port=8080)
